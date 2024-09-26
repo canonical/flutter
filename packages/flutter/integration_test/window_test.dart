f9e3e5921663306b058f7e809a14f0afea2d08cb
Matthew Kosarek <matt.kosarek@canonical.com>
2024-09-26 14:20:54 -0400

    update: able to test if we can create a regular window

--- packages/flutter/integration_test/window_test.dart
+++ packages/flutter/integration_test/window_test.dart
@@ -1,10 +1,38 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'dart:io' show Platform;
+import 'package:flutter/material.dart';
+
+void startApp() => runWidget(MultiWindowApp(initialWindows: [
+    (BuildContext context) => createRegularWindow(
+        context: context,
+        size: const Size(800, 600),
+        builder: (BuildContext context) {
+          return const MaterialApp(home: MyApp());
+        })
+  ]));
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Plugin example app'),
+      ),
+      body: Center(
+        child: Text('Platform: ${Platform.operatingSystem}\n'),
+      ),
+    );
+  }
+}
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets("failing test example", (WidgetTester tester) async {
-    expect(2 + 2, equals(5));
+  testWidgets("Test whether or not createRegularWindow will throw on this platform", (WidgetTester tester) async {
+    startApp();
+    await tester.pumpAndSettle();
   });
 }
