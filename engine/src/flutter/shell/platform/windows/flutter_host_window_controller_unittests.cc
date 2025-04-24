b913d31dc85e38003efe5d4d11635f20251ac82e
Harlen Batagelo <hbatagelo@gmail.com>
2025-04-24 10:38:12 -0300

    Add tests for dialogs; reformat and fix naming of existing tests

--- engine/src/flutter/shell/platform/windows/flutter_host_window_controller_unittests.cc
+++ engine/src/flutter/shell/platform/windows/flutter_host_window_controller_unittests.cc
@@ -44,14 +44,14 @@ class FlutterHostWindowControllerTest : public WindowsTest {
 
   int64_t engine_id() { return reinterpret_cast<int64_t>(engine_.get()); }
   flutter::Isolate& isolate() { return *isolate_; }
-  RegularWindowCreationRequest* creation_request() {
-    return &creation_request_;
+  RegularWindowCreationRequest* regular_window_creation_request() {
+    return &regular_window_creation_request_;
   }
 
  private:
   std::unique_ptr<FlutterWindowsEngine> engine_;
   std::optional<flutter::Isolate> isolate_;
-  RegularWindowCreationRequest creation_request_{
+  RegularWindowCreationRequest regular_window_creation_request_{
       .content_size =
           {
               .has_size = true,
@@ -73,8 +73,8 @@ TEST_F(FlutterHostWindowControllerTest, WindowingInitialize) {
       .on_message = [](WindowsMessage* message) { received_message = true; }};
 
   FlutterWindowingInitialize(engine_id(), &init_request);
-  const int64_t view_id =
-      FlutterCreateRegularWindow(engine_id(), creation_request());
+  const int64_t view_id = FlutterCreateRegularWindow(
+      engine_id(), regular_window_creation_request());
   DestroyWindow(FlutterGetWindowHandle(engine_id(), view_id));
 
   EXPECT_TRUE(received_message);
@@ -86,7 +86,7 @@ TEST_F(FlutterHostWindowControllerTest, HasTopLevelWindows) {
   bool has_top_level_windows = FlutterWindowingHasTopLevelWindows(engine_id());
   EXPECT_FALSE(has_top_level_windows);
 
-  FlutterCreateRegularWindow(engine_id(), creation_request());
+  FlutterCreateRegularWindow(engine_id(), regular_window_creation_request());
   has_top_level_windows = FlutterWindowingHasTopLevelWindows(engine_id());
   EXPECT_TRUE(has_top_level_windows);
 }
@@ -94,38 +94,113 @@ TEST_F(FlutterHostWindowControllerTest, HasTopLevelWindows) {
 TEST_F(FlutterHostWindowControllerTest, CreateRegularWindow) {
   IsolateScope isolate_scope(isolate());
 
-  const int64_t view_id =
-      FlutterCreateRegularWindow(engine_id(), creation_request());
+  const int64_t view_id = FlutterCreateRegularWindow(
+      engine_id(), regular_window_creation_request());
   EXPECT_EQ(view_id, 0);
+}
+
+TEST_F(FlutterHostWindowControllerTest, CreateModelessDialog) {
+  IsolateScope isolate_scope(isolate());
+
+  DialogWindowCreationRequest creation_request{
+      .content_size =
+          {
+              .has_size = true,
+              .width = 400,
+              .height = 300,
+          },
+      .parent_window = nullptr,
+  };
+
+  const int64_t view_id =
+      FlutterCreateDialogWindow(engine_id(), &creation_request);
+  EXPECT_EQ(view_id, 0);
+
+  const HWND window_handle = FlutterGetWindowHandle(engine_id(), view_id);
+  ASSERT_NE(window_handle, nullptr);
+  const HWND owner_window_handle = GetWindow(window_handle, GW_OWNER);
+  EXPECT_EQ(owner_window_handle, nullptr);
+}
+
+TEST_F(FlutterHostWindowControllerTest, CreateModalDialog) {
+  IsolateScope isolate_scope(isolate());
+
+  const int64_t regular_view_id = FlutterCreateRegularWindow(
+      engine_id(), regular_window_creation_request());
+  EXPECT_EQ(regular_view_id, 0);
+
+  const HWND regular_window_handle =
+      FlutterGetWindowHandle(engine_id(), regular_view_id);
+  ASSERT_NE(regular_window_handle, nullptr);
+
+  DialogWindowCreationRequest dialog_creation_request{
+      .content_size =
+          {
+              .has_size = true,
+              .width = 400,
+              .height = 300,
+          },
+      .parent_window = regular_window_handle,
+  };
+
+  const int64_t dialog_view_id =
+      FlutterCreateDialogWindow(engine_id(), &dialog_creation_request);
+  EXPECT_EQ(dialog_view_id, 1);
+
+  const HWND dialog_window_handle =
+      FlutterGetWindowHandle(engine_id(), dialog_view_id);
+  ASSERT_NE(dialog_window_handle, nullptr);
+  const HWND owner_window_handle = GetWindow(dialog_window_handle, GW_OWNER);
+  EXPECT_EQ(owner_window_handle, regular_window_handle);
 }
 
 TEST_F(FlutterHostWindowControllerTest, GetWindowHandle) {
   IsolateScope isolate_scope(isolate());
 
-  const int64_t view_id =
-      FlutterCreateRegularWindow(engine_id(), creation_request());
+  const int64_t view_id = FlutterCreateRegularWindow(
+      engine_id(), regular_window_creation_request());
   const HWND window_handle = FlutterGetWindowHandle(engine_id(), view_id);
   EXPECT_NE(window_handle, nullptr);
 }
 
-TEST_F(FlutterHostWindowControllerTest, GetWindowSize) {
+TEST_F(FlutterHostWindowControllerTest, GetWindowContentSize) {
   IsolateScope isolate_scope(isolate());
 
-  const int64_t view_id =
-      FlutterCreateRegularWindow(engine_id(), creation_request());
+  const int64_t view_id = FlutterCreateRegularWindow(
+      engine_id(), regular_window_creation_request());
   const HWND window_handle = FlutterGetWindowHandle(engine_id(), view_id);
 
   FlutterWindowSize size = FlutterGetWindowContentSize(window_handle);
 
-  EXPECT_EQ(size.width, creation_request()->content_size.width);
-  EXPECT_EQ(size.height, creation_request()->content_size.height);
+  EXPECT_EQ(size.width, regular_window_creation_request()->content_size.width);
+  EXPECT_EQ(size.height,
+            regular_window_creation_request()->content_size.height);
+}
+
+TEST_F(FlutterHostWindowControllerTest, SetWindowContentSize) {
+  IsolateScope isolate_scope(isolate());
+
+  const int64_t view_id = FlutterCreateRegularWindow(
+      engine_id(), regular_window_creation_request());
+  const HWND window_handle = FlutterGetWindowHandle(engine_id(), view_id);
+
+  FlutterWindowSizing requested_size{
+      .has_size = true,
+      .width = 640,
+      .height = 480,
+  };
+  FlutterSetWindowContentSize(window_handle, &requested_size);
+
+  FlutterWindowSize actual_size = FlutterGetWindowContentSize(window_handle);
+  EXPECT_EQ(actual_size.width, 640);
+  EXPECT_EQ(actual_size.height, 480);
 }
 
 TEST_F(FlutterHostWindowControllerTest, GetWindowState) {
   IsolateScope isolate_scope(isolate());
 
-  const int64_t view_id =
-      FlutterCreateRegularWindow(engine_id(), creation_request());
+  const int64_t view_id = FlutterCreateRegularWindow(
+      engine_id(), regular_window_creation_request());
   const HWND window_handle = FlutterGetWindowHandle(engine_id(), view_id);
   const int64_t window_state = FlutterGetWindowState(window_handle);
   EXPECT_EQ(window_state, static_cast<int64_t>(WindowState::kRestored));
@@ -134,40 +209,21 @@ TEST_F(FlutterHostWindowControllerTest, GetWindowState) {
 TEST_F(FlutterHostWindowControllerTest, SetWindowState) {
   IsolateScope isolate_scope(isolate());
 
-  const int64_t view_id =
-      FlutterCreateRegularWindow(engine_id(), creation_request());
+  const int64_t view_id = FlutterCreateRegularWindow(
+      engine_id(), regular_window_creation_request());
   const HWND window_handle = FlutterGetWindowHandle(engine_id(), view_id);
 
-  const std::array kWindowStates = {
+  const std::array window_states = {
       static_cast<int64_t>(WindowState::kRestored),
       static_cast<int64_t>(WindowState::kMaximized),
       static_cast<int64_t>(WindowState::kMinimized),
   };
 
-  for (const auto requested_state : kWindowStates) {
+  for (const auto requested_state : window_states) {
     FlutterSetWindowState(window_handle, requested_state);
     const int64_t actual_state = FlutterGetWindowState(window_handle);
     EXPECT_EQ(actual_state, requested_state);
   }
-}
-
-TEST_F(FlutterHostWindowControllerTest, SetWindowSize) {
-  IsolateScope isolate_scope(isolate());
-
-  const int64_t view_id =
-      FlutterCreateRegularWindow(engine_id(), creation_request());
-  const HWND window_handle = FlutterGetWindowHandle(engine_id(), view_id);
-
-  FlutterWindowSizing requestedSize{
-      .has_size = true,
-      .width = 640,
-      .height = 480,
-  };
-  FlutterSetWindowContentSize(window_handle, &requestedSize);
-
-  FlutterWindowSize actual_size = FlutterGetWindowContentSize(window_handle);
-  EXPECT_EQ(actual_size.width, 640);
-  EXPECT_EQ(actual_size.height, 480);
 }
 
 }  // namespace testing
