e3b1a23d76d53fa9e7c33fe066236309c6e7101c
Matthew Kosarek <matt.kosarek@canonical.com>
2025-08-06 08:38:02 -0400

    Initial tests for the DisplayManager

--- engine/src/flutter/shell/platform/windows/windows_proc_table.h
+++ engine/src/flutter/shell/platform/windows/windows_proc_table.h
@@ -11,7 +11,7 @@
 #include "flutter/fml/macros.h"
 #include "flutter/fml/native_library.h"
 
-namespace flutter {
+namespace flutter {  // namespace
 
 // Lookup table for Windows APIs that aren't available on all versions of
 // Windows, or for mocking Windows API calls.
